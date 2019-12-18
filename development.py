348e171d082b32775e16083447b0a7a9be1be4a9
q59494hj <huw.jones@manchester.ac.uk>
2019-12-18 19:20:29 +0000

    local changes

--- development.py
+++ development.py
@@ -8,6 +8,7 @@ from mpl_toolkits.mplot3d import Axes3D
 from sklearn.metrics import mean_squared_error, r2_score
 import numpy as np
 import itertools
+import sys
 
 import pickle
 
@@ -33,12 +34,106 @@ def features_and_labels(path_string, time=50, result="1"):
     X, Y = [], []
 
     for case in cases:
-        instance1 = ci(case)
+        instance = ci(case)
 
-        X.append(instance1.linear_crack_array_1d())
-        Y.append(instance1.get_result_at_time(time, result_columns=str(result)))
+        X.append(instance.linear_crack_array_1d())
+        Y.append(instance.get_result_at_time(time, result_columns=str(result)))
 
     return X, Y
+
+
+def result_time_history(case, result="1", time_steps=271):
+    """Gets a result time history for one case i.e. one result at each time"""
+
+    instance = ci(case)
+
+    time_history = []
+
+    for time in range(1, time_steps + 1):
+        time_history.append(instance.get_result_at_time(time, result_columns=str(result)))
+
+    return time_history
+
+
+with open('data.pkl', 'rb') as f:
+    X, Y_50_1, Y_50_2, x_coord_inter_channel, y_coord_inter_channel = pickle.load(
+        f)
+
+case_number = 50
+
+path_cases = '/media/huw/Seagate Expansion Drive/parmec_results/'
+
+cases = cases_list(path_cases)
+
+case = cases[case_number]
+
+case_name = case.split('/')[-1]
+
+regressor = LinearRegression()
+
+X = np.where(np.array(X) > 1, 1, np.array(X))
+
+cvr_1 = cross_val_predict(regressor, X, Y_50_1, cv=8)
+cvr_2 = cross_val_predict(regressor, X, Y_50_2, cv=8)
+
+axes = plt.gca()
+axes.set_xlim([min(Y_50_1[case_number])*0.9, max(Y_50_1[case_number])*1.1])
+axes.set_ylim([min(cvr_1[case_number])*0.9, max(cvr_1[case_number])*1.1])
+
+plt.title(case_name)
+plt.xlabel('Actual')
+plt.ylabel('Prediction')
+
+plt.scatter(Y_50_1[case_number], cvr_1[case_number], marker='o')
+plt.plot(Y_50_1[case_number], Y_50_1[case_number])
+plt.show()
+
+
+
+#
+# instance1 = ci(case)
+#
+# counts_local, counts_adjacent, counts_outer = [], [], []
+#
+# for i in range(1, instance1.last_channel(channel_type='inter') + 1):
+#     local, adjacent, outer = instance1.get_cracks_per_layer(str(i), array_type='pos', channel_type='inter',
+#                                                             inclusive=True)
+#
+#     counts_local.append(local)
+#     counts_adjacent.append(adjacent)
+#     counts_outer.append(outer)
+#
+# Y_time = result_time_history(case)
+#
+# for i, time_result in enumerate(Y_time):
+#
+#     plt.subplot(211)
+#
+#     title = "Instance: " + case_name + " - Concentration of Cracks "
+#     plt.title(title)
+#
+#     plt.scatter(x_coord_inter_channel, y_coord_inter_channel, c=counts_local, marker='o', label="Local")
+#
+#     plt.axis('off')
+#
+#     plt.subplot(212)
+#
+#     plt.scatter(x_coord_inter_channel, y_coord_inter_channel, c=time_result, marker='o',
+#             label="Magnitude of Displacement in x-direction (b = 1)")
+#
+#     title = "Instance: " + case_name + " - Result at timestep: " + str(i)
+#
+#     plt.title(title)
+#     # plt.xlabel('x (meters)')
+#     # plt.ylabel('y (meters)')
+#     # plt.legend(loc='lower center', bbox_to_anchor=(0.5, -0.35), shadow=True, ncol=2)
+#
+#     name = "./Figures1/" + case_name + "_i" + str(i) + '_r' + '1' + '.png'
+#
+#     plt.savefig(name)
+    # plt.show()
+
+
 
 
 # case_intact = '/home/huw/PycharmProjects/Results/position_identification/intact_core_rb'
@@ -48,55 +143,44 @@ def features_and_labels(path_string, time=50, result="1"):
 # x_coord_fuel, y_coord_fuel, z_coord_fuel = instance1.get_fuel_channel_xyz_positions()
 # x_coord_inter, y_coord_inter, z_coord_inter = instance1.get_interstitial_channel_xyz_positions()
 
-path_cases = '/media/huw/Seagate Expansion Drive/parmec_results/'
 
-cases = cases_list(path_cases)
+# counts_local, counts_adjacent, counts_outer = [], [], []
+#
+# for i in range(1, instance1.last_channel(channel_type='inter') + 1):
+#     local, adjacent, outer = instance1.get_cracks_per_layer(str(i), array_type='pos', channel_type='inter',
+#                                                             inclusive=True)
+#
+#     counts_local.append(local)
+#     counts_adjacent.append(adjacent)
+#     counts_outer.append(outer)
 
-instance1 = ci(cases[case_number])
+# j = 0
+#
+# i = 0
+# x_coord_inter_channel, y_coord_inter_channel = [], []
+# for x, y in zip(x_coord_inter, y_coord_inter):
+#     i += 1
+#     if i % 13 == 0:
+#         x_coord_inter_channel.append(x)
+#         y_coord_inter_channel.append(y)
 
-
-
-with open('objs.pkl', 'rb') as f:
-    X, Y_50_1, Y_50_2, x_coord_fuel, y_coord_fuel, z_coord_fuel, x_coord_inter, y_coord_inter, z_coord_inter = pickle.load(
-        f)
-
-j = 0
-
-i = 0
-x_coord_inter_channel, y_coord_inter_channel = [], []
-for x, y in zip(x_coord_inter, y_coord_inter):
-    i += 1
-    if i % 13 == 0:
-        x_coord_inter_channel.append(x)
-        y_coord_inter_channel.append(y)
-
-counts_local, counts_adjacent, counts_outer = [], [], []
-
-for i in range(1, instance1.last_channel(channel_type='inter') + 1):
-    local, adjacent, outer = instance1.get_cracks_per_layer(str(i), array_type='pos', channel_type='inter',
-                                                            inclusive=True)
-
-    counts_local.append(local)
-    counts_adjacent.append(adjacent)
-    counts_outer.append(outer)
 
 # X, Y_50_1 = features_and_labels(path_cases)
 # X, Y_50_2 = features_and_labels(path_cases, result="2")
 
-# with open('objs.pkl', 'wb') as f:
-#     pickle.dump([X, Y_50_1, Y_50_2, x_coord_fuel, y_coord_fuel, z_coord_fuel, x_coord_inter, y_coord_inter,
-#                  z_coord_inter], f)
+# with open('data.pkl', 'wb') as f:
+#     pickle.dump([X, Y_50_1, Y_50_2, x_coord_inter_channel, y_coord_inter_channel], f)
 
 # plt.scatter(x_coord_inter_channel, y_coord_inter_channel, c='black', marker='o',
 #             label="Interstitial Channels")
 
-plt.scatter(x_coord_inter_channel, y_coord_inter_channel, c=counts_local, marker='o', label="Local")
-plt.legend(loc='lower center', bbox_to_anchor=(0.5, -0.15), shadow=True, ncol=2)
-plt.show()
-
-plt.scatter(x_coord_inter_channel, y_coord_inter_channel, c=Y_50_1[case_number], marker='o', label="Result")
-plt.legend(loc='lower center', bbox_to_anchor=(0.5, -0.15), shadow=True, ncol=2)
-plt.show()
+# plt.scatter(x_coord_inter_channel, y_coord_inter_channel, c=counts_local, marker='o', label="Local")
+# plt.legend(loc='lower center', bbox_to_anchor=(0.5, -0.15), shadow=True, ncol=2)
+# plt.show()
+#
+# plt.scatter(x_coord_inter_channel, y_coord_inter_channel, c=Y_50_1[case_number], marker='o', label="Result")
+# plt.legend(loc='lower center', bbox_to_anchor=(0.5, -0.15), shadow=True, ncol=2)
+# plt.show()
 # print(x_coord_inter)
 # fig = plt.figure()
 # ax = Axes3D(fig)
@@ -146,7 +230,8 @@ plt.show()
 # plt.show()
 #
 # # X = np.where(np.array(X) > 1, 1, np.array(X))
-# #
+# #cvr = cross_val_predict(regressor, X, Y, cv=8)
+# # #     cross_val_results.append(cvr)
 # # train_results = []
 # #
 # # cross_val_results = []
@@ -158,8 +243,7 @@ plt.show()
 #
 # #     print('\n===\n', regressor, '\n')
 # #
-# #     cvr = cross_val_predict(regressor, X, Y, cv=8)
-# #     cross_val_results.append(cvr)
+# #
 # #
 # #     mse = mean_squared_error(Y, cvr)
 # #
