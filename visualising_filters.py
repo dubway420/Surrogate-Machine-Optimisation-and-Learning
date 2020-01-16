d302a7fbfa45b2a260c0114face06c8548ba70cd
Huw Rhys Jones <43721202+dubway420@users.noreply.github.com>
2020-01-16 18:26:56 +0000

    visualising filters

--- visualising_filters.py
+++ visualising_filters.py
@@ -1,42 +1,96 @@
-# load vgg model
-from keras.applications.vgg16 import VGG16
-from keras.applications.vgg16 import VGG16
-from matplotlib import pyplot
+from matplotlib import pyplot as plt
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, Flatten
+from keras.utils import to_categorical
+from keras.models import Model
+import math as m
+import numpy as np
 
-# load the model
-model = VGG16()
-# summarize the model
-model.summary()
+epochs = 10
 
-# load the model
-model = VGG16()
-# summarize filter shapes
-for layer in model.layers:
-    # check for convolutional layer
-    if 'conv' not in layer.name:
-        continue
-    # get filter weights
-    filters, biases = layer.get_weights()
-    print(layer.name, filters.shape)
+# download mnist data and split into train and test sets
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
 
-filters, biases = model.layers[1].get_weights()
+X_train = X_train.reshape(60000, 28, 28, 1)
+X_test = X_test.reshape(10000, 28, 28, 1)
 
-f_min, f_max = filters.min(), filters.max()
-filters = (filters - f_min) / (f_max - f_min)
+y_train = to_categorical(y_train)
+y_test = to_categorical(y_test)
 
-# plot first few filters
-n_filters, ix = 6, 1
-for i in range(n_filters):
-    # get the filter
-    f = filters[:, :, :, i]
-    # plot each channel separately
-    for j in range(3):
-        # specify subplot and turn of axis
-        ax = pyplot.subplot(n_filters, 3, ix)
-        ax.set_xticks([])
-        ax.set_yticks([])
-        # plot filter channel in grayscale
-        pyplot.imshow(f[:, :, j], cmap='gray')
-        ix += 1
-# show the figure
-pyplot.show()
+# create model
+conv2d = Sequential()
+conv2d.add(Conv2D(64, kernel_size=5, activation='sigmoid', input_shape=(28, 28, 1)))
+conv2d.add(Conv2D(32, kernel_size=5, activation='relu'))
+conv2d.add(Flatten())
+conv2d.add(Dense(10, activation='softmax'))
+
+conv2d.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+for layer in conv2d.layers:
+    print(layer.name)
+
+model = Model(inputs=conv2d.inputs, outputs=conv2d.layers[0].output)
+
+# conv2d.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=epochs)
+
+# conv2d.save_weights("conv2d.h5")
+conv2d.load_weights("conv2d.h5")
+
+
+case = 5000
+channel = 0
+
+plt.imshow(X_train[case].reshape(28, 28), cmap='gray')
+plt.show()
+feature_map = model.predict(X_train[case].reshape(1, X_train[0].shape[0], X_train[0].shape[1], X_train[0].shape[2]))
+
+print(feature_map.shape)
+
+
+plt.imshow(feature_map[0, :, :, channel], cmap='gray')
+plt.show()
+
+filter_no = 0
+
+filters, biases = conv2d.layers[0].get_weights()
+filter = filters[:, :, :, filter_no].reshape(filters.shape[0], filters.shape[1])
+
+plt.imshow(filter, cmap='gray')
+plt.show()
+#
+# # filter = filters[:, :, :, 1].reshape(filters.shape[0], filters.shape[1])
+# #
+# # plt.imshow(filter, cmap='gray')
+# # plt.show()
+#
+# f_min, f_max = filters.min(), filters.max()
+# filters = (filters - f_min) / (f_max - f_min)
+#
+# # # plot first few filters
+# n_filters, rows_cols = filters.shape[-1], int(m.sqrt(filters.shape[-1]))
+#
+# fig, axs = plt.subplots(rows_cols, rows_cols)
+#
+# for row_no in range(rows_cols):
+#
+#     # plot each channel separately
+#     for col_no in range(rows_cols):
+#
+#         filter_no = (row_no * rows_cols) + col_no
+#         filter = filters[:, :, :, filter_no].reshape(filters.shape[0], filters.shape[1])
+#
+#         filter = np.where(filter > 0.5, 1, filter)
+#         filter_Shreshold = np.where(filter <= 0.5, 0, filter)
+#
+#         # specify subplot and turn of axis
+#         # ax = plt.subplot(n_filters, 3, ix)
+#         ax = axs[row_no, col_no]
+#         ax.set_xticks([])
+#         ax.set_yticks([])
+#         # plot filter channel in grayscale
+#         ax.imshow(filter_Shreshold, cmap='Greys')
+#
+# # show the figure
+# plt.show()
+
