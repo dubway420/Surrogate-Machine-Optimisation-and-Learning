f944e5e5ef801c91172a1abd38e8bff351f6f5e4
q59494hj <huw.jones@manchester.ac.uk>
2020-03-16 15:09:49 +0000

    modified is_in function to allow multiple search terms

--- parmec_analysis/utils.py
+++ parmec_analysis/utils.py
@@ -10,20 +10,29 @@ def get_id_from_filename(filename):
     return os.path.splitext(os.path.basename(filename))[0]
 
 
-def is_in(string, search_term):
+def is_in(string, search_term, *args):
     """ This method tells the user if a search term is contained within another string.
     It's an extension of the 'in' qualifier, but does so for all capitalisations"""
 
     # Converts both input arguments to lowercase
     string_lowercase = string.lower()
-    search_term_lowercase = search_term.lower()
 
-    # checks if the lowercase search_string is in the string
+    # If there are any optional arguments, convert those to lower case too
+    terms_lowercase = [search_term.lower()]
+    for term in args:
+        if isinstance(term, str):
+            terms_lowercase.append(term.lower())
+        else:
+            print("skipping term: ", term, "which is not a valid string")
+
+    # checks if the lowercase search_string is in each string
     # If it is, return true, else false
-    if search_term_lowercase in string_lowercase:
-        return True
-    else:
-        return False
+
+    for search_term_lowercase in terms_lowercase:
+        if search_term_lowercase in string_lowercase:
+            return True
+
+    return False
 
 
 def directories_in_path(path):
