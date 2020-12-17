2073b8ec3e55e8bbed94cc2f986822d718face2b
Huw Jones <huw.jones@manchester.ac.uk>
2020-12-17 18:01:19 +0000

    experiment input files now have model checkpoint callbacks

--- experiment_input_files/experiment2.py
+++ experiment_input_files/experiment2.py
@@ -1,15 +1,45 @@
 from machine_learning.experiment import Experiment
 from experiment_input_files.trial_common_parameters import parameters
 from machine_learning.models import RegressionModels as RegMods
-from machine_learning.dataset_generators import Features1D as Features
+from machine_learning.dataset_generators import Features3D as Features
+from machine_learning.callbacks import LossHistory as History, lr_scheduler
+from keras.callbacks import LearningRateScheduler, ModelCheckpoint
+from sklearn import preprocessing as pre
+import os
+import ntpath
+
+checkpoint_filepath = os.path.splitext(ntpath.basename(__file__))[0] + ".{epoch:02d}-{val_loss:.2f}.hdf5"
+
+
+############################################
+# Model Checkpoint
+############################################
+
+model_checkpoint_callback = ModelCheckpoint(
+    filepath=checkpoint_filepath,
+    save_weights_only=False,
+    monitor='val_loss',
+    mode='min',
+    save_best_only=True)
+
+############################################
+
+#lrate = LearningRateScheduler(lr_scheduler, verbose=1)
+callbacks = [History, model_checkpoint_callback]
 
 dataset = parameters.dataset
 
-features = Features(parameters.dataset, extra_dimension=False)
+features = Features(parameters.dataset, array_type="Positions")
 
 labels = parameters.labels
 
-model = RegMods.multi_layer_perceptron(features.feature_shape, labels.label_shape, activation="linear",
-                                       layers=(32, 16, 8))
+min_max_scaler = pre.MinMaxScaler(feature_range=(0, 1))
 
-experiment = Experiment(parameters, "MLP_32_16_8_linear", model, dataset, features, labels)
+#features.transform(min_max_scaler)
+#labels.transform(min_max_scaler)
+
+model = RegMods.convolutional_neural_network_2d(features.feature_shape, labels.label_shape, activation="linear",
+                                                layers=(64, 256, 384, 384, 256, 128, 256), padding="same", kernel_shape=5)
+
+experiment = Experiment(parameters, "3D_64_256_2x384_256_128_256_ks5", model, dataset, features, labels,
+                        callbacks)
---
+++ experiment_input_files/experiment8.py
@@ -0,0 +1,45 @@
+from machine_learning.experiment import Experiment
+from experiment_input_files.trial_common_parameters import parameters
+from machine_learning.models import RegressionModels as RegMods
+from machine_learning.dataset_generators import Features3D as Features
+from machine_learning.callbacks import LossHistory as History, lr_scheduler
+from keras.callbacks import LearningRateScheduler, ModelCheckpoint
+from sklearn import preprocessing as pre
+import os
+import ntpath
+
+checkpoint_filepath = os.path.splitext(ntpath.basename(__file__))[0] + ".{epoch:02d}-{val_loss:.2f}.hdf5"
+
+
+############################################
+# Model Checkpoint
+############################################
+
+model_checkpoint_callback = ModelCheckpoint(
+    filepath=checkpoint_filepath,
+    save_weights_only=False,
+    monitor='val_loss',
+    mode='min',
+    save_best_only=True)
+
+############################################
+
+#lrate = LearningRateScheduler(lr_scheduler, verbose=1)
+callbacks = [History, model_checkpoint_callback]
+
+dataset = parameters.dataset
+
+features = Features(parameters.dataset, array_type="Positions")
+
+labels = parameters.labels
+
+min_max_scaler = pre.MinMaxScaler(feature_range=(0, 1))
+
+#features.transform(min_max_scaler)
+#labels.transform(min_max_scaler)
+
+model = RegMods.convolutional_neural_network_2d(features.feature_shape, labels.label_shape, activation="linear",
+                                                layers=(64, 256, 384, 384, 256, 256, 512), padding="same", kernel_shape=[11, 5, 3, 3, 3])
+
+experiment = Experiment(parameters, "3D_64_256_2x384_256_256_512_ks11_5_3_3_3", model, dataset, features, labels,
+                        callbacks)
