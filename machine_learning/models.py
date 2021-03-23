2605e3a22a4dadcc5fca7ddecc09e875535f10ac
Huw Rhys Jones <43721202+dubway420@users.noreply.github.com>
2021-03-23 22:33:25 +0000

    CNN_2d method overhaul: method for convo model can now vary the number of convo and dense layers independently. Drop out can now be specified on a per layer basis.

--- machine_learning/models.py
+++ machine_learning/models.py
@@ -35,11 +35,48 @@ def layer_activations_validation(layer_output_shapes, activation):
             for _ in layer_output_shapes[len(activations):]:
                 activations.append(activations[-1])
 
-    return layer_output_shapes, activations
-    
-    
-def kernels(number_convo_layers, kernel):
+    return activations
 
+
+def convo_dense_layers(layers):
+    """Takes a tuple or list - layers - and converts it into two tupes, one for convolutional layers - convo_layers -
+    and one for dense layers.
+    """
+
+    dense_layers = ()
+    dense_assigned = False
+
+    # If the last item is iterable, it is the dense layers
+    if iterable(layers[-1]):
+        dense_layers = layers[-1]
+        dense_assigned = True
+
+    # If the first item in the layers argument is iterable, it is the convo layers
+    if iterable(layers[0]):
+        convo_layers = layers[0]
+
+        # if dense_layers hasn't been set, then the last term isn't iterable. In this case, dense layers just becomes
+        # everything after the first item
+        if not dense_assigned:
+            dense_layers = layers[1:]
+
+    else:
+
+        # if dense layers isn't set, it's likely that it's just a tuple of all numbers. In this case, the last two
+        # numbers are used as the dense layers
+        if not dense_assigned:
+            convo_layers = layers[:-2]
+            dense_layers = layers[-2:]
+
+        # if the dense layers were the final item in the layers list, then everything up to that point is the convo
+        # layers
+        else:
+            convo_layers = layers[:-1]
+
+    return convo_layers, dense_layers
+
+
+def kernels(number_convo_layers, kernel):
     if not iterable(kernel):
 
         kernels_list = [kernel for _ in range(number_convo_layers)]
@@ -51,10 +88,26 @@ def kernels(number_convo_layers, kernel):
         if len(kernels_list) < number_convo_layers:
 
             for _ in range(len(kernels_list), number_convo_layers):
-
                 kernels_list.append(kernels_list[-1])
 
-    return kernels_list    
+    return kernels_list
+
+
+def dropout_validation(number_layers, fraction):
+    if not iterable(fraction):
+
+        kernels_list = [fraction for _ in range(number_layers)]
+
+    else:
+
+        kernels_list = list(fraction)
+
+        if len(kernels_list) < number_layers:
+
+            for _ in range(len(kernels_list), number_layers):
+                kernels_list.append(kernels_list[-1])
+
+    return kernels_list
 
 
 class RegressionModels:
@@ -83,22 +136,6 @@ class RegressionModels:
         # return our model
         return model
 
-    # 1
-    @staticmethod
-    def wider_model(input_dims, output_dims):
-
-        # create model
-        model = Sequential()
-
-        model.name = "Wider Perceptron"
-
-        model.add(Dense(20, input_dim=input_dims, kernel_initializer='normal', activation='tanh'))
-        model.add(Dense(4, activation="tanh"))
-        model.add(Dense(output_dims, kernel_initializer='normal'))
-        # Regularisation
-
-        return model
-
     # 2
     @staticmethod
     def cnn1D(input_dims, output_dims):
@@ -114,125 +151,60 @@ class RegressionModels:
 
         return model
 
-    @staticmethod
-    def cnn1D_k6(input_dims, output_dims):
-
-        # create model
-        model = Sequential()  # add model layers
-        model.add(Conv1D(64, kernel_size=6, activation='tanh', input_shape=input_dims))
-        model.add(Conv1D(32, kernel_size=3, activation='tanh'))
-        model.add(Flatten())
-        model.add(Dense(output_dims, activation='tanh'))
-
-        model.name = "CNN 1D"
-
-        return model
-
-    # 3
-    @staticmethod
-    def cnn2D_type1(input_dims, output_dims):
-        # create model
-        model = Sequential()  # add model layers
-        model.add(Conv2D(64, kernel_size=3, activation='tanh', input_shape=input_dims))
-        model.add(Conv2D(32, kernel_size=3, activation='tanh'))
-        model.add(Flatten())
-        model.add(Dense(output_dims, activation='tanh'))
-
-        # Name the neural network
-        model.name = "CNN 2D - 1"
-
-        # model.compile(loss='mean_squared_error', optimizer='adam')
-        return model
-
-    # 4
-    @staticmethod
-    def cnn2D_type2(input_dims, output_dims, filters=(16, 32, 64), activation="tanh", regress=True):
-
-        model = Sequential()  # add model layers
-        model.add(Conv2D(filters[0], kernel_size=3, activation=activation, input_shape=input_dims))
-
-        if len(filters) > 1:
-            for filter_dims in filters[1:]:
-                model.add(Conv2D(filter_dims, kernel_size=3, activation=activation))
-                model.add(BatchNormalization())  # axis=chan_dim
-
-        model.add(Flatten())
-        model.add(Dense(16, activation='tanh'))
-        model.add(Dropout(0.5))
-        model.add(Dense(4, activation='tanh'))
-
-        # check to see if the regression node should be added
-        if regress:
-            model.add(Dense(output_dims, activation="linear"))
-
-        # Name the neural network
-        model.name = "CNN 2D - 2"
-
-        # return the CNN
-        return model
-
     # 0
     @staticmethod
     def convolutional_neural_network_2d(input_dims, output_dims, activation="linear", layers=(16, 32, 64),
                                         regularizer=None, dropout=0.5, kernel_shape=3, padding="valid"):
 
-        layer_output_shapes, activations = layer_activations_validation(layers, activation)
-        
-        filters = kernels(len(layer_output_shapes) - 2, kernel_shape)
+        convo_layers, dense_layers = convo_dense_layers(layers)
+
+        layers_concatenated = convo_layers + dense_layers
+
+        activations = layer_activations_validation(layers_concatenated, activation)
+
+        filters = kernels(len(convo_layers), kernel_shape)
+
+        dropouts = dropout_validation(len(layers_concatenated), dropout)
 
         # define our MLP network
         model = Sequential()
 
-        model.name = "Convolutional Neural Network"
+        # model.name = "Convolutional Neural Network"
 
-        model.add(Conv2D(layer_output_shapes[0], kernel_size=filters[0], input_shape=input_dims,
-                         activity_regularizer=regularizer, padding=padding))
-        model.add(Activation(activations[0]))
+        # Input layer
+        model.add(Conv2D(convo_layers[0], kernel_size=filters[0], padding=padding, activity_regularizer=regularizer,
+                         input_shape=input_dims, activation=activations[0]))
 
-        for i, (layer_output_shape, act) in enumerate(zip(layer_output_shapes[1:-2], activations[1:-2])):
-            model.add(Conv2D(layer_output_shape, kernel_size=filters[i+1], padding=padding))
-            model.add(Activation(act))
+        if dropouts[0]:
+            model.add(Dropout(dropouts[0]))
+
+        layer_index = 1
+
+        # Convolutional layers are added here
+        for layer_output_shape in convo_layers[1:]:
+            model.add(Conv2D(layer_output_shape, kernel_size=filters[layer_index], padding=padding,
+                             activation=activations[layer_index]))
+
+            if dropouts[layer_index]:
+                model.add(Dropout(dropouts[layer_index]))
+
+            layer_index += 1
 
         model.add(Flatten())
 
-        model.add(Dense(layer_output_shapes[-2], activation=activations[-2]))
-        model.add(Dropout(dropout))
-        model.add(Dense(layer_output_shapes[-1], activation=activations[-1]))
+        # Dense layers are added here
+        for layer_output_shape in dense_layers:
+            model.add(Dense(layer_output_shape, activation=activations[layer_index]))
+
+            if dropouts[layer_index]:
+                model.add(Dropout(dropouts[layer_index]))
+
+            layer_index += 1
 
         model.add(Dense(output_dims))
-        model.add(Activation("linear"))
+        # model.add(Activation("linear"))
 
         # return our model
         return model
-        
+
         # 0
-    @staticmethod
-    def convolutional_neural_network_2d_no_fc(input_dims, output_dims, activation="linear", layers=(16, 32, 64),
-                                        regularizer=None, dropout=0.5, kernel_shape=3):
-
-        layer_output_shapes, activations = layer_activations_validation(layers, activation)
-
-        # define our MLP network
-        model = Sequential()
-
-        model.name = "Convolutional Neural Network"
-
-        model.add(Conv2D(layer_output_shapes[0], kernel_size=kernel_shape, input_shape=input_dims,
-                         activity_regularizer=regularizer))
-        model.add(Activation(activations[0]))
-
-        for layer_output_shape, act in zip(layer_output_shapes[1:-2], activations[1:-2]):
-            model.add(Conv2D(layer_output_shape, kernel_size=kernel_shape))
-            model.add(Activation(act))
-
-        model.add(Flatten())
-
-        model.add(Dense(layer_output_shapes[-2], activation=activations[-2]))
-        model.add(Dropout(dropout))
-        model.add(Dense(layer_output_shapes[-1], activation=activations[-1]))
-
-        model.add(Dense(output_dims))
-        model.add(Activation("linear"))
-
-        # return our model
-        return model    
