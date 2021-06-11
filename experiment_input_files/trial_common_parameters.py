bfa34d4be67fe62840f2964b3a99a8d596c52e62
Huw <huw.jones@manchester.ac.uk>
2021-06-11 21:43:15 +0100

    added seperate vgg_model method in regression models

--- experiment_input_files/trial_common_parameters.py
+++ experiment_input_files/trial_common_parameters.py
@@ -1,29 +1,29 @@
-from machine_learning.dataset_generators import DatasetSingleFrame, Labels
+from machine_learning.dataset_generators import DatasetSingleFrame, Displacements as Labels
 from machine_learning.trial import TrialParameters
-from keras.optimizers import RMSprop, Adam, Nadam
-from keras.losses import mean_squared_error as loss
+from tensorflow.keras.optimizers import RMSprop, Adam, Nadam
+from tensorflow.keras.losses import mean_squared_error as loss
 
 ###############################################################################
 # ######## FILL THIS IS WITH PARAMETERS COMMON TO THE ENTIRE TRIAL ############
 ###############################################################################
 
-trial_name = "exp3_p3"
+trial_name = "Reanalysis1"
 dataset_path = '~/'
 
 # Model parameters
-epochs = 200
+epochs = 300
 opt = Nadam(0.0001)
 loss = loss
 plot_every_n_epochs = int(epochs / 2)  # by default, prints 4 times during training
 
-# Features
+# Features - Note this is only for decoration/record: these variables affect nothing
 channels_features = 'all'
 levels_features = 'all'
 array_type = 'Positions'
 
 # Labels
-channels_labels = "all"
-levels_labels = 'all'
+channels_labels = "160"
+levels_labels = '12'
 result_type = 'all'
 result_time = 48
 result_column = 1
@@ -35,8 +35,8 @@ dataset = DatasetSingleFrame(dataset_path, number_of_cases=no_instances)
 # '
 
 labels = Labels(dataset, channels=channels_labels, result_time=result_time, result_type=result_type,
-                levels=levels_labels, unit="millimeters")
-
+                #levels=levels_labels, unit="millimeters")
+                levels=levels_labels)   
 ############################
 
 parameters = TrialParameters(trial_name, dataset, labels, epochs, opt, loss, plot_every_n_epochs)
