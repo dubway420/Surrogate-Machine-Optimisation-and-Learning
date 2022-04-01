171a49fe575aa62c5b2299bfaa0b4341929e184d
andrei.popescu-3@student.manchester.ac.uk <j27308ap@hlogin1.pri.csf3.alces.network>
2022-04-01 22:38:41 +0100

    Created a run file for the tests

---
+++ run_test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash --login
+#$ -cwd               # Run the job in the current directory
+#$ -l nvidia_v100=1
+#$ -t 1-32
+
+# Load modules
+module load apps/binapps/anaconda3/2019.07               # Python 3.7.3
+module load libs/cuda
+module load apps/binapps/keras/2.2.4-tensorflow-gpu      # GPU version of tensorflow 1.14.0
+module load libs/intel-18.0/hdf5/1.10.5_mpi              # Intel 18.0.3 compiler, OpenMPI 4.0.1
+
+#export OMP_NUM_THREADS=$NSLOTS
+
+python test_single.py
+
+#echo "Experiment $experiment_number complete."
+date
+  
+  
+
+
