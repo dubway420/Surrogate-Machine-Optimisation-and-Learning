2427e04f302e8daac209a9729d1e17d4ec445bf7
Huw <huw.jones@manchester.ac.uk>
2022-06-13 15:43:38 +0100

    Version 0.1 Added changelog.txt Also - experiment_summary.py now lists the framework version and the version of various packages used.

--- machine_learning/experiment_summary.py
+++ machine_learning/experiment_summary.py
@@ -1,4 +1,8 @@
 from keras.utils.vis_utils import plot_model
+import tensorflow as tf
+import tensorflow.keras as tfk
+import numpy as np
+import matplotlib as mpl
 
 
 def summary(experiment):
@@ -7,6 +11,12 @@ def summary(experiment):
     model = experiment.model
 
     output_file_path = experiment.trial.trial_name + "/" + experiment.name + "/" + experiment.name
+
+    f = open('changelog.txt')
+
+    version = f.readline()
+
+    f.close()
 
     with open(output_file_path + ".info", 'w') as fh:
 
@@ -33,6 +43,32 @@ def summary(experiment):
             for term in config:
                 fh.write(term + ": " + str(config[term]) + "\n")
             fh.write("\n")
+
+        fh.write("\n--------------------\n")
+
+        fh.write("This model was trained using the Machine Learning Surrogate Model Framework by H.Rhys Jones\n")
+        fh.write("Copyright 2022\n")
+        fh.write("contact: huw.jones@manchester.ac.uk\n")
+
+        fh.write("\n")
+        fh.write(version)
+        fh.write("\n")
+        message = "Tensorflow version: " + tf.__version__
+        fh.write(message)
+        fh.write("\n")
+        message = "Keras version:" + tfk.__version__
+        fh.write(message)
+        fh.write("\n")
+        message = "Numpy version: " + np.__version__
+        fh.write(message)
+        fh.write("\n")
+        message = "Matplotlib version: " + mpl.__version__
+        fh.write(message)
+
+
+
+
+
 
     plot_file_name = output_file_path + ".png"
     # plot_model(model, to_file=plot_file_name, show_shapes=True, show_layer_names=True)
