9d25d9e2e626aa9b8f6c59841a7159fb70ace382
H. Rhys Jones <huw.jones@manchester.ac.uk>
2022-10-06 16:20:02 +0100

    Fully set up automatd testing

--- machine_learning/utils.py
+++ machine_learning/utils.py
@@ -1,3 +1,4 @@
+from os import listdir
 import numpy as np
 
 
@@ -67,3 +68,54 @@ def augmentation_handler(variables):
             print("augmentations need to be specified in the form <augmentation>=<value1>_<value2>_...\n")
 
     return flip, rotate
+
+
+def experiment_number_finder(search_name, directory):
+    """ Find the experiment number of the experiment with the given name in the given directory 
+    
+    search_name (string): this is the name of the experiment you are trying to find
+
+    directory (string): this is the folder that contains the files to search through. This is usually 
+    the parent file of the trial
+
+    Returns: experiment number (integer), this is the experiment number of the experiment you are 
+    trying to find. Taken from the file name i.e. if the search_name was found in experiment3.py, this
+    method will return 3.
+
+    """
+
+
+
+    # Get the list of files in the directory
+    path = directory + "/experiment_input_files"
+    files = listdir(path)
+
+
+    # Find the experiment number
+    # Do this by iterating through the files in the experiment_input_files directory
+    for file in files:
+
+        # If the file has "experiment" in the title, then it is a experimentX.py files
+        if is_in(file, "experiment"): 
+
+            # Get just the number of the experiment i.e. remove the string "experiment" and the 
+            # extension ".py"
+            experiment_number = file.replace("experiment", "").replace(".py", "")
+
+            # Read all lines of the experimentX.py file until the experiment name label is found
+            with open(path + "/" + file, "r") as f:
+                lines = f.readlines()
+
+                for line in lines:
+
+                    # This is the identifying line
+                    if is_in(line, "experiment_name = "):
+
+                        # Get just the name of the experiment and stop searching through this 
+                        # particular file 
+                        experiment_name = line.split("=")[1].strip()
+                        break
+
+            # If the file carries the correct experiment name, return the experiment number            
+            if is_in(experiment_name, search_name):
+                return experiment_number     
--- test_single.py
+++ test_single.py
@@ -5,35 +5,57 @@ from machine_learning.callbacks import correlation_foursquare, histogram_foursqu
 from tensorflow.keras.losses import mean_squared_error as mse
 import numpy as np
 from os import listdir, mkdir
-from machine_learning.utils import is_in, augmentation_handler
+from machine_learning.utils import is_in, augmentation_handler, experiment_number_finder
 import sys
 from openpyxl import load_workbook
 
-dataset = DatasetSingleFrame(name="test_set")
+variables = sys.argv[1:]
 
-#inputs = CracksPlanar(dataset, extra_dimension=True, levels="5-7")
-inputs = Cracks3D(dataset, array_type="Positions", levels="5-7")
-#inputs = Cracks3D(dataset, array_type="Orient", levels="5-7")
-#inputs = Cracks1D(dataset, array_type="Positions", levels="5-7")
+path = variables[0]
 
-# Labels
-channels_labels = "160"
-levels_labels = '12'
-result_type = 'all'
-result_time = 48
-result_column = 1
+# If the last character of path is a slash remove it
+if path[-1] == "/":
+    path = path[:-1]
 
-no_instances = 'all'
 
-labels = Displacements(dataset, channels=channels_labels, result_time=result_time, result_type=result_type,
-                       # levels=levels_labels, unit="millimeters")
-                       levels=levels_labels)
+name = path.split("/")[-2] + "_" + path.split("/")[-1]
+
+experiment_name = path.split("/")[-1]
+
+trial_name = path.split("/")[-2]
+
+exp_number = experiment_number_finder(experiment_name, trial_name)
+
+package = trial_name + ".experiment_input_files.experiment" + exp_number
+
+experiment = getattr(__import__(package, fromlist=["experiment"]), "experiment")(trial_name)
+
+dataset = experiment.dataset
+inputs = experiment.features
+labels = experiment.labels
+# dataset = DatasetSingleFrame(name="test_set")
+
+# #inputs = CracksPlanar(dataset, extra_dimension=True, levels="5-7")
+# inputs = Cracks3D(dataset, array_type="Positions", levels="5-7")
+# #inputs = Cracks3D(dataset, array_type="Orient", levels="5-7")
+# #inputs = Cracks1D(dataset, array_type="Positions", levels="5-7")
+
+# # Labels
+# channels_labels = "160"
+# levels_labels = '12'
+# result_type = 'all'
+# result_time = 48
+# result_column = 1
+
+# no_instances = 'all'
+
+# labels = Displacements(dataset, channels=channels_labels, result_time=result_time, result_type=result_type,
+#                        # levels=levels_labels, unit="millimeters")
+#                        levels=levels_labels)
 
 transformer_name = labels.generate_filename(ext=".tfr")
 
 transformer_name_dataset = transformer_name.replace("test_set", "dataset")
-
-variables = sys.argv[1:]
 
 
 if len(variables) >= 2:
@@ -46,6 +68,9 @@ if len(variables) >= 2:
 
     transformer_name_dataset = transformer_name_dataset.replace(".tfr", aug_name)
 
+# Remove any existing transformation
+labels.inverse_transform()
+
 print("Transformer name: ", transformer_name_dataset)
 
 
@@ -55,23 +80,7 @@ with open(transformer_name_dataset, 'rb') as f:
     # min_max_scaler = pre.MinMaxScaler(feature_range=(0, 1))
 labels.transform(transformer, fit=False, save=False)
 
-#########################################################
-# Replace this with the path to your download of the experiment
-#########################################################
-path = variables[0]
 
-# If the last character of path is a slash remove it
-if path[-1] == "/":
-    path = path[:-1]
-
-print("Path: ", path)
-
-#########################################################
-# Replace this with the name of the folder you want to save your test results to
-#########################################################
-# folder_name = "CustomLossMeanRot2Flip1"
-
-name = path.split("/")[-2] + "_" + path.split("/")[-1]
 # if name == "":
     # name = path.split("/")[-2] + "_" + path.split("/")[-1]
 
